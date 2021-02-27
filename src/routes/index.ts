e1b28594e26fc7defcc496fb186f3a187abb3f44
Carlos Alexandre <carlos11alexandre11@gmail.com>
2021-02-27 11:25:40 -0300

    Estrutura do projeto.

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import sessionsRouter from './sessions.routes';
+import usersRouter from './user.routes';
+
+const routes = Router();
+
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
+
+export default routes;
